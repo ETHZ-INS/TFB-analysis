9f694ddb7d413f4948be80fd34493f89c6ea5412
emsonder <emanuel.sonder@hest.ethz.ch>
2025-08-04 19:45:00 +0200

    Restrict batch3 to TFs with 3 / 4 training contexts

--- 03_training_prediction/runPredictions_batch_3.R
+++ 03_training_prediction/runPredictions_batch_3.R
@@ -6,7 +6,7 @@ seed <- 43
 metaData <- readRDS("data/01_maeAll_meta.rds")
 metaData <- subset(metaData, is_matched)
 metaAg <- metaData[,.(n_con=length(unique(full_id))), by=tf_name]
-tfNamesBatch <- setdiff(unique(subset(metaAg, n_con>=3)$tf_name), "CTCF")
+tfNamesBatch <- unique(subset(metaAg, n_con>=3 & n_con<=4)$tf_name)
 
 outBaseDir <- "data/predictions"
 cofactors <- readRDS("data/meta_data/topInteractors.rds")
